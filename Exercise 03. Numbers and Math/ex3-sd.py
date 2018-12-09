0f2c8d1cd6a798b19ae491b624a501b3347d7745
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2018-12-09 13:13:21 -0600

    Exercise 03 Done!

---
+++ Exercise 03. Numbers and Math/ex3-sd.py
@@ -0,0 +1,49 @@
+# Exercise 03: Numbers and Math - Study Drills.
+
+# Prints the message "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# Prints "Hens" and the result of (25 + (30 / 6))
+print("Hens", 25.0 + 30.0 / 6.0)
+
+# Prints "Roosters" and the result of (100 - (25 * (3 % 4)))
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints "Now I will count the eggs:"
+print("Now I will count the eggs:")
+
+# Prints the result of (3 + 2 + 1 -5 + (4 % 2) - (1 / 4) + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints "Is it true that 3 + 2 < 5 - 7?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints the result of comparing (3 + 2) < (5 - 7) with a Boolean
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints "What is 3 + 2?" and the result of (3 + 2)
+print("What is 3 + 2?", 3.0 + 2.0)
+
+# Prints "What is 5 - 7?" and the result of (5 - 7)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Prints "Oh, that's why it's False."
+print("Oh, that's why it's False.")
+
+# Prints "How about some more."
+print("How about some more.")
+
+# Prints "Is it greater?" and the result of comparing (5 > -2)
+print("Is it greater?", 5.0 > -2.0)
+
+# Prints "Is it greater or equal?" and the result of comparing (5 >= -2)
+print("Is it greater or equal?", 5.0 >= -2.0)
+
+# Prints "Is it less or equal?" and the result of comparing  (5 <= -2)
+print("Is it less or equal?", 5.0 <= -2.0)
+
+# Personal calculations
+
+print("Christmas gifts:", 295 + 340)
+print("Steam games:", 349.50 + 299.25 + 349.50 + 187.19)
+print("Credit card fee:", 1500 / 12)
