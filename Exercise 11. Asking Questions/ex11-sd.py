5a42ae0b2856ae0024288b7be60304e0dbf1e1a0
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2018-12-09 19:17:10 -0600

    Exercise 11 Done!

---
+++ Exercise 11. Asking Questions/ex11-sd.py
@@ -0,0 +1,16 @@
+# Exercise 11. Asking Questions - Study Drills
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+SW_fan = input("Do you like Star Wars? ")
+music = input("Which music genres do you like? ")
+videogame = input("Which is your favorite videogame? ")
+
+print(f"\nSo, you're {age} old, {height} tall and {weight} heavy.")
+print(f'''
+Personal stuff:
+Star Wars fan: {SW_fan}.
+Music genres: {music}.
+"Favorite videogame: {videogame}
+''')
