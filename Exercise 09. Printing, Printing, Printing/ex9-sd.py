e3b777e12db1c08b0fbd14d452096d54789bd304
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2018-12-09 18:38:41 -0600

    Exercise 09 Done!

---
+++ Exercise 09. Printing, Printing, Printing/ex9-sd.py
@@ -0,0 +1,20 @@
+# Exercise 09. Printing, Printing, Printing - Study Drills
+
+# Here's some new strange stuff, remember type it exactly
+
+# String separated by a blank space
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Sting separated by a new line character (\n)
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Printing the strings
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Multiline string
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
