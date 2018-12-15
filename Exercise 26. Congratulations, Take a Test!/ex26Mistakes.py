6c3a7bd86271e0d4d05c7607e29f79973c039c49
AlejandroCamara <21253054+AlejandroCamara@users.noreply.github.com>
2018-12-14 23:35:18 -0600

    Exercise 26 Done!

---
+++ Exercise 26. Congratulations, Take a Test!/ex26Mistakes.py
@@ -0,0 +1,124 @@
+# Exercise 26. Congratulations, Take a Test! - Original Code
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+# TODO: Close the parenthesis and correct typo
+print("How much do you weigh?", end=' '
+weight = input()
+
+# TODO: Define height previously
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# TODO: Import from sys module
+script, filename = argv
+
+# TODO: Correct the variable name
+txt = open(filenme)
+
+# TODO: Add the f flag
+print("Here's your file {filename}:")
+# TODO: Correct the variable name
+print(tx.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+# TODO: Correct read function call
+print(txt_again_read())
+
+# TODO: Close the files
+
+# TODO: Escape the single quote
+print('Let's practice everything.')
+# TODO: Create a multiline string or adapt it into one line
+print('You\'d need to know \'bout escapes 
+      with \\ that do \n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+# TODO: Close the quotes
+print("--------------)
+print(poem)
+# TODO: Open the quotes
+print(--------------")
+
+# TODO: Add the missing number
+five = 10 - 2 + 3 - 
+
+# TODO: Close the parenthesis
+print(f"This should be five: {five}"
+
+# TODO: Add the colon
+def secret_formula(started)
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    # TODO: Add an math operand
+    crates = jars  100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+# TODO: Add the crates variable to unpack
+beans, jars = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+
+# TODO: Replace with /= operand
+start_point = start_point / 10
+
+print("We can also do that this way:")
+# TODO: Correct the variable typo
+formula = secret_formula(startpoint)
+# this is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+
+
+
+people = 20
+# TODO: Correct the typo
+cates = 30
+dogs = 15
+
+
+if people < cats:
+    # TODO: Add parentheses
+    print "Too many cats! The world is doomed!"
+
+# TODO: Correct the comparison operand
+if people < cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+# TODO: Add the colon
+if people > dogs
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+# TODO: Add the colon
+if people <= dogs
+    # TODO: Close the quotes
+    print("People are less than or equal to dogs.)
+
+# TODO: Correct the comparison operand
+if people = dogs:
+    print("People are dogs.")
